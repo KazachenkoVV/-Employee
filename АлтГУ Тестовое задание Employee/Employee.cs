445a1d0053585f8649a84562cdc4f77dbb8d1541
KazachenkoVV <Kazachenko.Valery.V@gmail.com>
2020-11-02 21:36:32 +1000

    Готовое задание.

---
+++ АлтГУ Тестовое задание Employee/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace АлтГУ_Тестовое_задание_Employee
+{
+    // Класс сотрудинки
+    class Employee
+    {
+        internal string FirstName { get; private set; }       // Имя
+        internal string MiddleName { get; private set; }      // Отчество
+        internal string SecondName { get; private set; }      // Фамилия
+        internal EmpType Empl { get; private set; }           // Должность
+        internal Department Department { get; private set; }  // Отдел
+        internal decimal Salary { get; private set; }         // Оклад
+
+        internal Employee(string firstName,
+                          string middleName,
+                          string secondName,
+                          EmpType empl,
+                          Department department,
+                          decimal salary)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            SecondName = secondName;
+            Empl = empl;
+            Department = department;
+            Salary = salary;
+        }
+
+        // Вывод карточки сотрудника
+        internal void DisplayEmployee()
+        {
+            Console.WriteLine("Карточка сотрудника\n");
+            Console.WriteLine($"Фамилия   - {SecondName}");
+            Console.WriteLine($"Имя       - {FirstName}");
+            Console.WriteLine($"Отчество  - {MiddleName}\n");
+            Console.WriteLine($"Должность - {Empl}");
+            Console.WriteLine($"Отдел     - {Department}");
+            Console.WriteLine($"Оклад     - {Salary:C2}");
+            Console.WriteLine("------------------------\n");
+        }
+    }
+}
---
+++ АлтГУ Тестовое задание Employee/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace АлтГУ_Тестовое_задание_Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Курс \"Разработка интернет - приложений на платформе C# ASP.Net MVC\"");
+            Console.WriteLine("Входное задание - “Класс «Сотрудник»”");
+            Console.WriteLine("Студент - Казаченко Валерий");
+            Console.WriteLine("02 ноября 2020\n\n");
+
+            Employee employee1 = new Employee("Семён", "Семёныч", "Горбунков", EmpType.директор, Department.Администраця, 100000m);
+            Employee employee2 = new Employee("Пётр", "Иванович", "Перестукин", EmpType.грузчик, Department.Склад, 31856m);
+
+            employee1.DisplayEmployee();
+            employee2.DisplayEmployee();
+
+            Console.Read();
+        }
+    }
+}
